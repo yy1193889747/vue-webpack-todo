f19749917f06506baeeec6ba777a146bf9cb45f1
yy1193889747 <1193889747@qq.com>
2018-01-31 10:34:16 +0800

    webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/images/pic.jpg'
+import './assets/style/test.css'
+
 const root = document.createElement('div')
 document.body.appendChild(root)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,37 @@
-webpack.config.js
+var webpack = require('webpack');
+const path = require('path')
+
+module.exports = {
+	entry: path.join(__dirname,'src/index.js'),
+	output:{
+		filename:"bundle.js",
+		path:path.join(__dirname,'dist')
+	},
+	module: {
+		rules: [
+		{
+			test: /\.vue$/,
+			loader: 'vue-loader'
+		},
+		{
+			test: /\.css$/,
+			use: [
+			'style-loader',
+			'css-loader'
+			]
+		},
+		{
+			test: /\.(gif|jpg|jpeg|png|svg)$/,
+			use: [{
+				loader: 'url-loader',
+				options: {
+					limit: 1024,
+					name: '[name]-aaa.[ext]'
+				}
+
+			}
+			]
+		}
+		]
+	}
+}
