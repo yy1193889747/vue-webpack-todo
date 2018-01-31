8dc089953febfcd6e738cd0b6adb50d30fbdb9b4
yy1193889747 <1193889747@qq.com>
2018-01-31 09:45:57 +0800

    webpack

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,20 @@
 <template>
-	
+<div id="test">{{text}}</div>	
 </template>
 
 <script type="text/javascript">
 export default {
-
-
+	data(){
+		return {
+			text:'abc'
+		}
+	}
 }
 	
 </script>
 
 <style type="text/css">
-	
+#test{
+	color: red;
+}	
 </style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-index.js
+import Vue from 'vue'
+import App from './app.vue'
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+new vue({
+	render: (h) => h(App)
+}).$mount(root)
